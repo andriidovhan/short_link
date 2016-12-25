b605397a19b27fe3bf8b0479c2f6a279ba8d1384
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-12-25 14:18:25 +0200

    change securerandom.safeurl to own gen_safe_link method

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,12 +15,18 @@ class LinksController < ApplicationController
 
   private
   def calc_n
-    return 3 if Link.all.count <= 100
-    return 4 if Link.all.count <= 1000
-    return 5 if Link.all.count <= 10000
+    return 2 if Link.all.count <= 3249
+    return 3 if Link.all.count <= 185193
+    return 5 if Link.all.count <= 10_556_001
+  end
+
+  def generate_safe_link(n)
+    ['q','w','e','r','t','y','u','p','a','s','d','f','g','h','j','k','z','x','c','v','b','n','m',\
+    'Q','W','E','R','T','Y','U','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',\
+    '2','3','4','5','6','7','8','9','-','_'].sample(n).join
   end
 
   def link_params
-    {origin: params[:origin], shorten: SecureRandom.urlsafe_base64(calc_n)}
+    {origin: params[:origin], shorten: generate_safe_link(calc_n)}
   end
 end
